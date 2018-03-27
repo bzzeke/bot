286896b6b08f9f1cca1b9c156b5ecdd9362fc071
Ilya Shalnev <zeke@cs-cart.com>
2018-03-27 13:00:11 +0800

    [~] Endpoint to process email2url messages was added.

--- index.php
+++ index.php
@@ -34,4 +34,38 @@ $app->match('/hook/process', function () use ($app, $telegram) {
     return 'ok';
 });
 
+$app->match('/email_received', function () use ($app, $telegram) {
+
+    $message = \bashkarev\email\Parser::email(file_get_contents('php://input'));
+    $chat_ids = Bot\ChatStorage::get();
+
+    $i = 0;
+    $text = $message->textPlain();
+    $attachments = $message->getAttachments();
+
+    if (!empty($attachments)) {
+        foreach ($attachments as $attachment) {
+            $file = tempnam('/tmp/', 'att_' . $i);
+            $attachment->save($file);
+
+            foreach ($chat_ids as $chat_id => $_data) {
+                Longman\TelegramBot\Request::sendPhoto([
+                    'chat_id' => $chat_id,
+                    'caption' => $text
+                ], $file);
+            }
+        }
+    } else {
+        foreach ($chat_ids as $chat_id => $_data) {
+            Longman\TelegramBot\Request::sendMessage([
+                'chat_id' => $chat_id,
+                'text' => $text
+            ]);
+        }
+    }
+
+    return 'ok';
+});
+
+
 $app->run();
