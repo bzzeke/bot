001dcbd9097f4fb31f14f43a4d8e9165a2a03904
Ilya Shalnev <zeke@cs-cart.com>
2018-06-27 10:38:02 +0400

    [~] Some MQTT topics were renamed. Fixed.

--- app/Telegram/Commands/TempCommand.php
+++ app/Telegram/Commands/TempCommand.php
@@ -14,7 +14,7 @@ use Longman\TelegramBot\Request;
 use Bot\ChatStorage;
 
 /**
- * User "/forcereply" command
+ * User "/temp" command
  */
 class TempCommand extends UserCommand
 {
@@ -28,54 +28,57 @@ class TempCommand extends UserCommand
     protected $text = '';
 
     protected $topics = array(
-        'Floor_1' => null,
-        'Floor_2' => null,
-        'Basement' => null,
-        'Floor 1 Temperature' => null,
-        'Floor 2 Temperature' => null,
-        'Basement Temperature' => null,
-        'Garret Temperature' => null,
-        'Outside Temperature' => null,
-        'Boiler Out Temperature' => null,
-        'Bath House Temperature' => null,
-        'Simple' => null,
-        'K8' => null,
-        'K1' => null,
-        'K2' => null,
-        'K3' => null,
-        'Water heater' => null,
-        'Work time' => null,
-        'Pressure' => null,
-        'Enabled' => null
+        '/devices/thermostat/controls/Floor_1' => 'floor1',
+        '/devices/thermostat/controls/Floor_2' => 'floor2',
+        '/devices/thermostat/controls/Basement' => 'basement',
+        '/devices/thermostat/controls/Floor 1 Temperature' => 'floor1_temp',
+        '/devices/thermostat/controls/Floor 2 Temperature' => 'floor2_temp',
+        '/devices/thermostat/controls/Basement Temperature' => 'basement_temp',
+        '/devices/thermostat/controls/Garret Temperature' => 'garret_temp',
+        '/devices/thermostat/controls/Outside Temperature' => 'outside_temp',
+        '/devices/thermostat/controls/Boiler Out Temperature' => 'boiler_temp',
+        '/devices/thermostat/controls/Bath House Temperature' => 'bathhouse_temp',
+        '/devices/thermostat/controls/Simple' => 'simple',
+        '/devices/thermostat/controls/Work time' => 'work_time',
+        '/devices/thermostat/controls/Pressure' => 'pressure',
+        '/devices/thermostat/controls/Enabled' => 'enabled',
+        '/devices/wb-mr14_32/controls/K8' => 'boiler_relay',
+        '/devices/wb-mr14_32/controls/K1' => 'floor2_pump',
+        '/devices/wb-mr14_32/controls/K2' => 'floor1_pump',
+        '/devices/wb-mr14_32/controls/K3' => 'basement_pump',
+        '/devices/water_supply/controls/Heater' => 'water_heater',
+        '/devices/water_supply/controls/Pressure' => 'water_pressure',
     );
+    protected $processed_topics = [];
 
     protected $formatted_msg = <<<EOT
-Floor 1: *[K2]*, [Floor 1 Temperature]˚C → [Floor_1]˚C
-Floor 2: *[K1]*, [Floor 2 Temperature]˚C → [Floor_2]˚C
-Basement: *[K3]*, [Basement Temperature]˚C → [Basement]˚C
-Boiler: *[K8]*, [Boiler Out Temperature]˚C
-Simple mode: *[Simple]*
-Enabled: *[Enabled]*
-Pressure: [Pressure] bar
-Boiler work time: [Work time]
+Floor 1: *[floor1_pump]*, [floor1_temp]˚C → [floor1]˚C
+Floor 2: *[floor2_pump]*, [floor2_temp]˚C → [floor2]˚C
+Basement: *[basement_pump]*, [basement_temp]˚C → [basement]˚C
+Boiler: *[boiler_relay]*, [boiler_temp]˚C
+Simple mode: *[simple]*
+Enabled: *[enabled]*
+Pressure: [pressure] bar
+Boiler work time: [work_time]
 
-Garret: [Garret Temperature]˚C
-Outside: [Outside Temperature]˚C
-Bath house: [Bath House Temperature]˚C
+Garret: [garret_temp]˚C
+Outside: [outside_temp]˚C
+Bath house: [bathhouse_temp]˚C
 
-Water heater: *[Water heater]*
+Water heater: *[water_heater]*
+Water pressure: [water_pressure] bar
 
 EOT;
 
     protected $placeholder_types = [
-        'K1' => 'bool',
-        'K2' => 'bool',
-        'K3' => 'bool',
-        'K8' => 'bool',
-        'Simple' => 'bool',
-        'Enabled' => 'bool',
-        'Water heater' => 'bool',
-        'Work time' => 'hours'
+        'floor2_pump' => 'bool',
+        'floor1_pump' => 'bool',
+        'basement_pump' => 'bool',
+        'boiler_relay' => 'bool',
+        'simple' => 'bool',
+        'enabled' => 'bool',
+        'water_heater' => 'bool',
+        'work_time' => 'hours'
     ];
 
     /**#@-*/
@@ -98,8 +101,13 @@ EOT;
                 'qos' => 0,
                 'function' => array($this, 'processmsg')
             ),
+            '/devices/water_supply/#' => array(
+                'qos' => 0,
+                'function' => array($this, 'processmsg')
+            ),
         );
 
+        $this->processed_topics = array_fill_keys(array_keys($this->topics), null);
         $this->config['mqtt']->subscribe($topics, 0);
 
         $time_start = time();
@@ -125,27 +133,24 @@ EOT;
 
     public function processmsg($topic, $msg)
     {
-        $topic_array = explode('/', $topic);
-        $topic = array_pop($topic_array);
+        if (array_key_exists($topic, $this->processed_topics)) {
+            $this->processed_topics[$topic] = $msg;
 
-        if (array_key_exists($topic, $this->topics)) {
-            $this->topics[$topic] = $msg;
-
-            if (isset($this->placeholder_types[$topic])) {
-                switch ($this->placeholder_types[$topic]) {
+            if (isset($this->placeholder_types[$this->topics[$topic]])) {
+                switch ($this->placeholder_types[$this->topics[$topic]]) {
                     case 'bool':
-                        $this->topics[$topic] = (int)$msg == 0 ? 'off' : 'on';
+                        $this->processed_topics[$topic] = (int)$msg == 0 ? 'off' : 'on';
                     break;
 
                     case 'hours':
-                        $this->topics[$topic] = (int)$msg > 0 ? sprintf('%.1f hr', $msg / 60 / 60) : 'n/a';
+                        $this->processed_topics[$topic] = (int)$msg > 0 ? sprintf('%.1f hr', $msg / 60 / 60) : 'n/a';
                     break;
                 }
             }
         }
 
         $text = array();
-        foreach ($this->topics as $t => $val) {
+        foreach ($this->processed_topics as $t => $val) {
             if (is_null($val)) {
                 return;
             }
@@ -153,7 +158,7 @@ EOT;
 
         $this->text = strtr($this->formatted_msg, array_combine(array_map(function($v) {
             return '[' . $v . ']';
-        }, array_keys($this->topics)), $this->topics));
+        }, array_values($this->topics)), $this->processed_topics));
 
         $this->config['mqtt']->close();
     }
