e063ee0b409a54ddc6a8b8b6c09fc926e24a6180
Ilya Shalnev <zeke@cs-cart.com>
2019-08-21 16:27:49 +0400

    [~] CD: remote/local server support.

--- app/app/Bot/Controllers/Cd.php
+++ app/app/Bot/Controllers/Cd.php
@@ -6,15 +6,29 @@ use GuzzleHttp\Client;
 
 class Cd extends Controller
 {
-    public function hook()
+    public function remote()
+    {
+        return $this->runBuild(getenv("CD_REMOTE_SERVER"));
+    }
+
+    public function local()
+    {
+        return $this->runBuild(getenv("CD_LOCAL_SERVER"));
+    }
+
+    protected function runBuild($cd_server)
     {
         $payload = $this->request->getContent();
 
         if (!empty($payload) && $decoded = json_decode($payload)) {
-            $client = new Client();
-            $client->post(getenv("CD_SERVER"), [
-                'json' => $decoded
-            ]);
+            try {
+                $client = new Client();
+                $client->post($cd_server, [
+                    'json' => $decoded
+                ]);
+            } catch (\Exception $e) {
+                return $e->getMessage();
+            }
         }
 
         return "OK";
